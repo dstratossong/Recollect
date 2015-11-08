7d069b6ed789633ba043bad911f49e1f0ea4e0ce
Arshan Alam <arshan@arshanalam.com>
2015-11-08 00:34:06 -0500

    Added Camera capture functionality to UI

--- www/js/new-controller.js
+++ www/js/new-controller.js
@@ -6,20 +6,4 @@ angular.module('recollect')
 })
 
 .controller('NewCtrl', function ($scope, Camera) {
-
-  $scope.getPhoto = function () {
-    var options = {
-      quality: 50,
-      // targetWidth: 320,
-      // targetHeight: 320,
-      correctOrientation: true,
-      saveToPhotoAlbum: true
-    };
-
-    Camera.getPicture(options).then(function (imageURI) {
-      $scope.lastPhoto = imageURI;
-    }, function (err) {
-      alert("Error! " + err);
-    });
-  };
 });
